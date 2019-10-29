bfb81321906e5dc622aff16e445dc3d3bd652d63
S3A1 <S3A1@KCSF1305.KCS-Learning.local>
2019-10-29 14:50:48 +0900

    sssssssss

--- src/pac1/Test3.java
+++ src/pac1/Test3.java
@@ -1,9 +1,5 @@
 package pac1;
 
 public class Test3 {
-<<<<<<< HEAD
-	//3ds
-=======
-
->>>>>>> branch 'dev6' of https://github.com/shojimak/Road-to-Boss.git
+//aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 }
